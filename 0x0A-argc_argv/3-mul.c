5a09f9d6f3e26f548af42ec1e3a9ce187970f890
MMarzoo <mmarzouk12378@gmail.com>
2023-10-27 19:32:26 -0700

    a

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int main(int argc, char *agrv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	esle
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
