794551b3b5481260738166bd5bff154afccfae06
MMarzoo <mmarzouk12378@gmail.com>
2024-02-01 07:14:14 -0800

    creating file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-	unsigned long int i;
+	unsigned long int a;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
 	hash_table->size = size;
-	hash_table->array = malloc(sizeof(hash_node_t *));
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
+	for (a = 0; a < size; a++)
+		hash_table->array[a] = NULL;
 	return (hash_table);
 }
